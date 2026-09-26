f803d8676e199dfd8ec58708ca3f2a6180fc5922
bobaso <yoshioka.mwork@gmail.com>
2026-09-26 09:49:44 +0900

    Update work.js

--- work/work.js
+++ work/work.js
@@ -45,7 +45,7 @@ function resizeWorkCanvas() {
     ------------------------------------- */
 
 const scale =
-    Math.min(scaleX, scaleY) * 0.5;
+    Math.min(scaleX, scaleY) * 0.95;
 
 
     /* -------------------------------------
